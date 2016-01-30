6bb5699bcc6ddf157951235f2f2ee69af809a0c9
Yoann Moinet <yoann.moinet@gmail.com>
2016-01-30 11:39:08 -0500

    add files

---
+++ test/index.spec.js
@@ -0,0 +1,10 @@
+describe('dotconf', function () {
+    'use strict';
+
+    var chai = require('chai');
+    var expect = chai.expect;
+
+    it('should have tests', function () {
+        expect(0).to.equal(1);
+    });
+});
