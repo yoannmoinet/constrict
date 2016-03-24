c69eb20c5019973415f869f281020967137beea2
Yoann Moinet <yoann.moinet@gmail.com>
2016-03-23 23:21:56 -0400

    feat: add core functions constrict and liberate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs-extra');
 var path = require('path');
 var glob = require('glob');
+var crypto = require('crypto-js');
 
 var nb = {
     directories: 0,
@@ -20,7 +21,7 @@ function getLog (opts, log) {
             if (!opts || !opts.silent) {
                 console.error(error);
             }
-            throw new Error(error);
+            throw error instanceof Error ? error : new Error(error);
         },
         fatal: function (error) {
             if (!opts || !opts.silent) {
@@ -35,17 +36,27 @@ function getLog (opts, log) {
 
 var log = getLog();
 
-function decode (data) {
+function decode (data, passphrase) {
     try {
-        return decodeURIComponent(data);
+        var decoded;
+        if (passphrase && passphrase !== '') {
+            decoded = crypto.AES.decrypt(data.toString(), passphrase)
+                .toString(crypto.enc.Utf8);
+        }
+        decoded = decodeURIComponent(decoded || data);
+        return decoded;
     } catch (e) {
         log.error(e);
     }
 }
 
-function encode (data) {
+function encode (data, passphrase) {
     try {
-        return encodeURIComponent(data);
+        var encoded = encodeURIComponent(data);
+        if (passphrase && passphrase !== '') {
+            encoded = crypto.AES.encrypt(encoded, passphrase);
+        }
+        return encoded;
     } catch (e) {
         log.error(e);
     }
@@ -60,8 +71,9 @@ function get (pattern, opts) {
 }
 
 function read (fileName, cwd) {
+    fileName = cwd ? path.join(cwd, fileName) : fileName;
     try {
-        return fs.readFileSync(path.join(cwd, fileName)).toString();
+        return fs.readFileSync(fileName).toString();
     } catch (e) {
         log.error(e);
     }
@@ -96,7 +108,7 @@ function safe (fileName, cwd, files) {
 
 function write (fileName, cwd, data) {
     try {
-        fs.writeJsonSync(path.join(cwd, fileName), data);
+        fs.writeJson(path.join(cwd, fileName), data);
     } catch (e) {
         log.error(e);
     }
@@ -122,6 +134,101 @@ function putBack(fileName, cwd, type, data) {
     }
 }
 
+function walk (ar, action) {
+    if (ar instanceof Array) {
+        ar.forEach(function (item, index) {
+            action(item, index);
+        });
+    } else {
+        for (var i in ar) {
+            if (ar.hasOwnProperty(i)) {
+                action(ar[i], i);
+            }
+        }
+    }
+}
+
+function loopConstrict (files, ignore, store, passphrase) {
+    var opts, absolutePath;
+    store = store || {};
+    walk(files, function (file) {
+        // Only save files, not directories.
+        if (!fs.statSync(file).isDirectory()) {
+            nb.files += 1;
+            store[file] = {
+                content: encode(read(file), passphrase)
+            };
+        } else {
+            nb.directories += 1;
+        }
+    });
+    return store;
+}
+
+function merge (destination, store) {
+    var previousStore = {};
+    try {
+        previousStore = fs.readJsonSync(destination);
+    } catch (e) {
+        // No previous file.
+        return store;
+    }
+    return Object.assign(previousStore, store);
+}
+
+function constrict (opts) {
+    // Control options
+    if (!opts || (!opts.pattern && !opts.files)) {
+        log.error(
+            'Missing options "pattern" and / or "files"'
+        );
+        return;
+    }
+
+    if (opts.files && !opts.files instanceof Array) {
+        log.error(
+            'Wrong type option "files", must be an array'
+        );
+        return;
+    }
+
+    // Get the files to constrict together.
+    var files = opts.files || [];
+    if (opts.pattern) {
+        var opts = {
+            ignore: opts.ignore,
+            dot: true
+        };
+        files = files.concat(get(opts.pattern, opts));
+    }
+
+    // If we don't have any files... we can't do anything
+    if (files.length <= 0) {
+        log.fatal(
+            'No file selected, nothing to constrict.'
+        );
+    }
+
+    // Return the json constricted.
+    var output = loopConstrict(
+        files,
+        opts.ignore,
+        opts.passphrase
+    );
+
+    // If we have a destination we write
+    // the output in the file.
+    if (opts.destination) {
+        write(opts.destination, opts.cwd, output);
+    }
+
+    return output;
+}
+
+function liberate (fileName) {
+    var files = fs.readJsonSync(fileName);
+}
+
 module.exports = {
     decode: decode,
     encode: encode,
@@ -132,5 +239,7 @@ module.exports = {
     read: read,
     remove: remove,
     safe: safe,
-    write: write
+    write: write,
+    constrict: constrict,
+    liberate: liberate
 };
